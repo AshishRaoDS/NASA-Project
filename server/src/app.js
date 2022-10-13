7d07d83a85290b979523ba8c95a0e3d272774d3e
AshishRaoDS <ashraods@gmail.com>
2022-10-14 01:10:18 +0530

    adding scripts to run server and client at the same time

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,8 @@ const express = require("express")
 
 const cors = require("cors")
 
+const path = require("path")
+
 const planetRouter = require("./routes/planets/planets.router")
 
 const app = express()
@@ -10,6 +12,12 @@ app.use(cors({
   origin: "http://localhost:3000"
 }))
 app.use(express.json())
+app.use(express.static(path.join(__dirname, "..", "public")))
 app.use(planetRouter)
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"))
+})
+
 
 module.exports = app
